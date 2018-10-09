c8ec772c8794d79ebe41ed39bdf96f33ed8ccdb6
victorioc <victoriocabrera@gmail.com>
2018-10-09 14:35:55 -0400

    oops! typo in the associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   has_many :authors, through: :post_authors
 
   has_many :post_tags
-  has_many, :tags, through: :post_tags
+  has_many :tags, through: :post_tags
 end
