318c872a0c1356fb2a432321a6775fc577b00495
hallumse <hetoria@yahoo.com>
2018-10-10 16:55:35 -0400

    author creation works

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -10,19 +10,27 @@ class Admin::PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
+    @authors = Author.all
   end
 
   def create
-
+    Post.create(title:params[:post][:title], body:params[:post][:body], authors:[Author.find(params[:post][:id])])
+    redirect_to admin_posts_path
   end
 
   def edit
-
+    @post = Post.find params[:id]
   end
 
   def update
-
+   @post = Post.find params[:id]
+   @post.title = params[:post][:title]
+   @post.authors = [Author.find(params[:post][:authors])]
+   @post.body = params[:post][:body]
+   @post.tags = Tag.find(params[:post][:tag_ids])
+   @post.save
+   redirect_to posts_path
   end
 
   def destroy
